d3d21f8616fa715bdbee53ea1890ad74872c94ef
inesrubik <ines.rubik09@gmail.com>
2026-05-21 14:26:06 +0200

    attribute definiert

--- src/Medium.java
+++ src/Medium.java
@@ -1,2 +1,14 @@
-public class Medium {
+import java.util.List;
+
+public abstract class Medium {
+    private String title;
+    private int year;
+    private String category;
+    private List<Integer> raiting;
+    private boolean isBorrowed;
+    private int borCount;
+    private int medCount;
+    private String orLanguage;
+
+
 }
