bde244a5dc3ace207407d76ea516d196e291a1f0
kerstin12345 <4201@htl.rennweg.at>
2026-06-09 14:58:01 +0200

    javadoc der gemeinsamen Instanz

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -12,7 +12,6 @@ public class Main extends Application {
     private LibraryManager libraryManager = new LibraryManager();
 
 
-
     public static void main(String[] args) {
         launch(args);
     }
